209f7a215637ee4ef2519a6bdb16cc543e992b88
naoisemcgarrigle <nmgarr@outlook.com>
2025-11-26 21:01:30 +0000

    Commit 01 (Init)

---
+++ src/main/java/dev/naoisemcg/naoisespetitions/NaoisespetitionsApplication.java
@@ -0,0 +1,13 @@
+package dev.naoisemcg.naoisespetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaoisespetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaoisespetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/naoisemcg/naoisespetitions/NaoisespetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naoisemcg.naoisespetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaoisespetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
